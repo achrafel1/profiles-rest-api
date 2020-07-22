8fba50caa513a3e6553805e030f65b8d3f3c39c1
achraf elkhanjari <achrafelkhanjari99@gmail.com>
2020-07-22 21:02:39 +0100

    Added super profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,9 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
 from profiles_api import serializers
@@ -130,7 +131,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
-    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user."""
